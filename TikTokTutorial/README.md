decb3e3964a6080218d23f5d8368aef98527b7fe
Jun Gu <druidream@gmail.com>
2024-03-26 10:44:06 +0800

    Project Setup

---
+++ TikTokTutorial/README.md
@@ -0,0 +1,2 @@
+# TikTokTutorial
+ 
---
+++ TikTokTutorial/TikTokTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialApp.swift
+//  TikTokTutorial
+//
+//  Created by Jun Gu on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
