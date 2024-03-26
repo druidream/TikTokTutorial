db366152fd04925b53e6671b72ff9d078d6922c9
Jun Gu <druidream@gmail.com>
2024-03-26 11:55:01 +0800

    Profile View

---
+++ TikTokTutorial/Core/Profile/Components/UserStatView.swift
@@ -0,0 +1,31 @@
+//
+//  UserStatView.swift
+//  TikTokTutorial
+//
+//  Created by Jun Gu on 3/26/24.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+
+    let value: Int
+    let title: String
+
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.bold)
+
+            Text(title)
+                .font(.caption)
+                .foregroundStyle(.gray)
+        }
+        .frame(width: 80, alignment: .center)
+    }
+}
+
+#Preview {
+    UserStatView(value: 1, title: "")
+}
---
+++ TikTokTutorial/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,31 @@
+//
+//  CurrentUserProfileView.swift
+//  TikTokTutorial
+//
+//  Created by Jun Gu on 3/26/24.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 2) {
+                    // profile header
+                    ProfileHeaderView()
+
+                    // post grid view
+                    PostGridView()
+                }
+                .padding(.top)
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
